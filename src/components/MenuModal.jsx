1d8a0bcb522b6682eb7a850cc0d3c7ec34c374db
banuprakash92 <banuprakash.banu@gmail.com>
2023-10-17 00:53:10 -0400

    Add delete menu items

--- src/components/MenuModal.jsx
+++ src/components/MenuModal.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const MenuModal = ({ restaurant, menuItems, onClose }) => {
     const [quantities, setQuantities] = useState({});
     const [isAddMenuItemDialogOpen, setIsAddMenuItemDialogOpen] = useState(false);
+    const [searchId, setSearchId] = useState(''); // For searching by ID
+    const [localMenuItems, setLocalMenuItems] = useState(menuItems);
+
     // State for the new menu item form
     const [newMenuItem, setNewMenuItem] = useState({
         name: '',
@@ -14,19 +18,63 @@ const MenuModal = ({ restaurant, menuItems, onClose }) => {
         calories: ''
     });
 
+    const BASE_URL = "https://us-central1-maristhungerexpress.cloudfunctions.net/api";
+
     const handleOpenAddMenuItemDialog = () => {
+        setNewMenuItem(prevState => ({
+            ...prevState,
+            restaurant_id: restaurant._id // Prepopulate the restaurant_id field
+        }));
         setIsAddMenuItemDialogOpen(true);
     };
 
-    const handleCloseAddMenuItemDialog = () => {
+    const handleCloseAddMenuItemDialog = () => {const deleteMenuItem = async (menuItemId) => {
+        try {
+            const response = await axios.delete(`${BASE_URL}/menuItems/delete/${menuItemId}`);
+            console.log(response.data);
+            // Update local state by filtering out the deleted item
+            setLocalMenuItems(prevItems => prevItems.filter(item => item._id !== menuItemId));
+        } catch (error) {
+            console.error("Error deleting menu item:", error);
+        }
+    };
+    
         setIsAddMenuItemDialogOpen(false);
     };
 
-    const handleSaveMenuItem = (newMenuItem) => {
-    // Here, you can update the state and/or make an API call to save the new menu item to the backend.
-    console.log(newMenuItem);
-};
+    const handleSaveMenuItem = async (newMenuItem) => {
+        try {
+            const response = await axios.post(`${BASE_URL}/menuItems/create`, newMenuItem);
+            console.log(response.data);
+            fetchAllMenuItemsForRestaurant(restaurant._id); // Refresh the menu items after saving
+        } catch (error) {
+            console.error("Error creating menu item:", error);
+        }
+    };
 
+    useEffect(() => {
+        fetchAllMenuItemsForRestaurant(restaurant._id);
+    }, [restaurant]);
+
+    const fetchMenuItemById = async (menuItemId) => {
+        try {
+            const response = await axios.get(`${BASE_URL}/menuItems/menu/${menuItemId}`);
+            console.log(response.data);
+            // Set the data to your state or handle as needed
+        } catch (error) {
+            console.error("Error fetching menu item:", error);
+        }
+    };
+
+    const fetchAllMenuItemsForRestaurant = async (restaurantId) => {
+        try {
+            const response = await axios.get(`/menuItems/restaurant/${restaurantId}`);
+            console.log(response.data);
+            setLocalMenuItems(response.data); // Update the local state with the fetched menu items
+        } catch (error) {
+            console.error("Error fetching menu items for restaurant:", error);
+        }
+    };
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setNewMenuItem(prevState => ({
@@ -36,15 +84,48 @@ const MenuModal = ({ restaurant, menuItems, onClose }) => {
     };
 
     const handleSubmitMenuItem = () => {
-        handleSaveMenuItem(newMenuItem);
+        if (newMenuItem._id) {
+            // If the newMenuItem has an _id, it means it's an existing item and we're editing it
+            editMenuItem(newMenuItem._id, newMenuItem);
+        } else {
+            // Otherwise, it's a new item and we're creating it
+            handleSaveMenuItem(newMenuItem);
+        }
         handleCloseAddMenuItemDialog();
     };
+
+    const editMenuItem = async (menuItemId, updatedData) => {
+        try {
+            const response = await axios.put(`${BASE_URL}/menuItems/update/${menuItemId}`, updatedData);
+            console.log(response.data);
+            // Refresh your menu items or handle the response as needed
+        } catch (error) {
+            console.error("Error updating menu item:", error);
+        }
+    };
+
     const updateQuantity = (itemId, amount) => {
         setQuantities(prevQuantities => {
             const currentQuantity = prevQuantities[itemId] || 0;
             const updatedQuantity = Math.max(0, currentQuantity + amount);
             return { ...prevQuantities, [itemId]: updatedQuantity };
         });
+    };
+
+    const deleteMenuItem = async (menuItemId) => {
+        try {
+            const response = await axios.delete(`${BASE_URL}/menuItems/delete/${menuItemId}`);
+            console.log(response.data);
+            // Update local state by filtering out the deleted item
+            setLocalMenuItems(prevItems => prevItems.filter(item => item._id !== menuItemId));
+        } catch (error) {
+            console.error("Error deleting menu item:", error);
+        }
+    };
+    
+    const handleEditMenuItem = (item) => {
+        setNewMenuItem(item); // Populate the form with the item's details
+        handleOpenAddMenuItemDialog(); // Open the dialog
     };
 
     const handleAddToCart = (itemName) => {
@@ -55,9 +136,17 @@ const MenuModal = ({ restaurant, menuItems, onClose }) => {
         <div className="menu-modal-overlay">
             <div className="menu-modal">
                 <button className="close-button" onClick={onClose}>&times;</button>
-                <button onClick={handleOpenAddMenuItemDialog}>Add Menu Item</button>
+                <button className="add-menu-item-button" onClick={handleOpenAddMenuItemDialog}>Add Menu Item</button>
+                <input 
+                    type="text" 
+                    placeholder="Enter menu item ID" 
+                    value={searchId} 
+                    onChange={(e) => setSearchId(e.target.value)} 
+                />
+                <button className="search-id-button" onClick={() => fetchMenuItemById(searchId)}>Search by ID</button>
                 <h2>Menu Items for {restaurant.name}</h2>
-                {menuItems.map(item => (
+                <div className="menu-item-list">
+                    {localMenuItems.map(item => (
                         <div key={item._id} className="menu-item-details">
                             <div className="quantity-control">
                                 <button onClick={() => updateQuantity(item._id, -1)}>-</button>
@@ -73,15 +162,22 @@ const MenuModal = ({ restaurant, menuItems, onClose }) => {
                                 <p>Calories: {item.calories}</p>
                             </div>
                             <div className="menu-item-actions">
+                                <button className="edit-button" onClick={() => handleEditMenuItem(item)}>Edit</button>
+                                <button className="delete-button" onClick={() => deleteMenuItem(item._id)}>Delete</button>
                                 <button className="add-to-cart-button" onClick={() => handleAddToCart(item.name)}>Add to Cart</button>
                             </div>
                         </div>
                     ))}
+                </div>
             </div>
             {isAddMenuItemDialogOpen && (
                 <div className="add-menu-item-overlay">
                     <div className="add-menu-item-dialog">
                         <h3>Add New Menu Item</h3>
+                        <label>
+                        Restaurant ID:
+                        <input type="text" name="restaurant_id" value={newMenuItem.restaurant_id} disabled /> {/* Display the restaurant_id as a disabled input field */}
+                         </label>
                         <label>
                             Name:
                             <input type="text" name="name" value={newMenuItem.name} onChange={handleInputChange} />
@@ -92,7 +188,7 @@ const MenuModal = ({ restaurant, menuItems, onClose }) => {
                         </label>
                         <label>
                             Price:
-                            <input type="number" name="price" value={newMenuItem.price} onChange={handleInputChange} />
+                            <input type="number" step="0.01" name="price" value={newMenuItem.price} onChange={handleInputChange} />
                         </label>
                         <label>
                             Availability:
