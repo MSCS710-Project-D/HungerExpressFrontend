715b95d99780776812d96ec8cadce4f1a1fa754b
banuprakash92 <banuprakash.banu@gmail.com>
2023-11-10 00:00:38 -0500

    zip code validations

--- src/components/AddressInputRow.jsx
+++ src/components/AddressInputRow.jsx
@@ -1,16 +1,31 @@
+import React, { useState } from 'react';
+import { TextField, FormControl, InputLabel, Select, MenuItem, FormHelperText } from '@mui/material';
 
-import React from 'react';
-import { TextField } from '@mui/material';
 
-function AddressInputRow({ address, onChange }) {
+
+const US_STATES = [
+    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+    "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+    "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+    "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+    "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+];
+
+const isValidZipCode = (zipCode) => {
+    // Example: US zip code validation (5 digits or 9 digits with a hyphen)
+    const pattern = /^[0-9]{5}(?:-[0-9]{4})?$/;
+    return pattern.test(zipCode);
+};
+
+function AddressInputRow({ address, onChange, errors }) {
     return (
         <div className="address-input-row">
-            <TextField 
-                label="Address Type" 
-                name="name" 
-                value={address.name} 
-                onChange={onChange} 
-                required 
+            <TextField
+                label="Address Type"
+                name="name"
+                value={address.name}
+                onChange={onChange}
+                required
             />
             <TextField
                 label="First Name"
@@ -55,16 +70,25 @@ function AddressInputRow({ address, onChange }) {
                 name="zipCode"
                 value={address.zipCode}
                 onChange={onChange}
+                error={!!errors.zipCode}
+                helperText={errors.zipCode || ''}
                 required
             />
-            <TextField
-                className="mui-text-field"
-                label="State"
-                name="state"
-                value={address.state}
-                onChange={onChange}
-                required
-            />
+            <FormControl fullWidth>
+                <InputLabel id="state-select-label">State</InputLabel>
+                <Select
+                    labelId="state-select-label"
+                    name="state"
+                    value={address.state}
+                    onChange={onChange}
+                    label="State"
+                >
+                    {US_STATES.map((state, index) => (
+                        <MenuItem key={index} value={state}>{state}</MenuItem>
+                    ))}
+                </Select>
+                {/* Include FormHelperText if you have validation messages */}
+            </FormControl>
         </div>
     );
 }
--- src/components/AddressModal.jsx
+++ src/components/AddressModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+import { Button, Dialog, DialogTitle, DialogContent, DialogActions, FormControl, InputLabel, Select, MenuItem, FormHelperText } from '@mui/material';
 import AddressInputRow from './AddressInputRow'; // Adjust the path as per your directory structure
 import SavedAddresses from './SavedAddresses';
 import { useAddress } from './AddressContext'; // Adjust the path as per your directory structure
@@ -17,6 +17,24 @@ function AddressModal({ isOpen, onClose }) {
         state: ''
     });
 
+    const [errors, setErrors] = useState({
+        zipCode: '' // Initialize with empty string or appropriate default value
+    });
+
+    const isValidZipCode = (zipCode) => {
+        // Example: US zip code validation (5 digits or 9 digits with a hyphen)
+        const pattern = /^[0-9]{5}(?:-[0-9]{4})?$/;
+        return pattern.test(zipCode);
+    };
+
+    const US_STATES = [
+        "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", 
+        "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", 
+        "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", 
+        "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", 
+        "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+    ];
+
     const handleAddressChange = (e) => {
         setAddress({
             ...address,
@@ -24,25 +42,44 @@ function AddressModal({ isOpen, onClose }) {
         });
     };
 
+    
     const handleSaveAddress = () => {
-        addAddress(address);
-        setAddress({
-            name: '',
-            firstName: '',
-            lastName: '',
-            address1: '',
-            address2: '',
-            city: '',
-            zipCode: '',
-            state: ''
-        }); // Reset the form
+        // Implement validation logic here
+        let newErrors = {...errors};
+
+        // Example: Validate Zip Code
+        if (!isValidZipCode(address.zipCode)) {
+            newErrors.zipCode = 'Invalid Zip Code';
+        } else {
+            newErrors.zipCode = '';
+        }
+
+        setErrors(newErrors);
+
+        // Check if there are any errors before proceeding
+        const hasErrors = Object.values(newErrors).some(error => error !== '');
+        if (!hasErrors) {
+            addAddress(address);
+
+            setAddress({
+                name: '',
+                firstName: '',
+                lastName: '',
+                address1: '',
+                address2: '',
+                city: '',
+                zipCode: '',
+                state: ''
+            }); // Reset the form
+        }
+        
     };
 
     return (
         <Dialog open={isOpen} onClose={onClose}>
             <DialogTitle>Manage Addresses</DialogTitle>  
             <DialogContent>
-                <AddressInputRow address={address} onChange={handleAddressChange} />
+            <AddressInputRow address={address} onChange={handleAddressChange} errors={errors} />
                 <SavedAddresses addresses={savedAddresses} />
             </DialogContent>
             <DialogActions>
